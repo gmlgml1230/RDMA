482455036a0557a8360448da5169b45b5c54315c
hsjang <36947676+gmlgml1230@users.noreply.github.com>
2019-02-18 10:55:12 +0900

    Update RDMA.R
    수정

--- R/RDMA.R
+++ R/RDMA.R
@@ -12,11 +12,11 @@
 #' @import openxlsx
 
 RDMA <- function(){
-
+  
   options(httr_oauth_cache=T)
   # Shiny에서 5MB의 제한을 잡아놓은걸 30MB로 늘린 것
   options(shiny.maxRequestSize = 30 * 1024 ^ 2)
-
+  
   oauth_ck <- function(auth_file){
     if(file.exists(auth_file)){
       return("OK")
@@ -24,7 +24,7 @@ RDMA <- function(){
       return("NO")
     }
   }
-
+  
   oauth_trycatch <- function(auth_file, exr){
     if(file.exists(auth_file)){
       tryCatch({
@@ -35,7 +35,7 @@ RDMA <- function(){
       })
     }
   }
-
+  
   element_null_ck <- function(..., element_name, text_page, exr){
     null_ck <- vapply(list(...), is.null, TRUE)
     if(any(null_ck)){
@@ -44,12 +44,12 @@ RDMA <- function(){
       exr
     }
   }
-
+  
   oauth_trycatch("sc.httr-oauth", {
     gar_auth("sc.httr-oauth")
     website_url <- searchConsoleR::list_websites()$siteUrl
   })
-
+  
   ui <- miniPage(
     tags$script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-117525726-1', 'auto');ga('send', 'pageview')"),
     tags$style("
@@ -58,11 +58,11 @@ RDMA <- function(){
                "
     ),
     gadgetTitleBar("Data Preparation Tool"),
-
+    
     miniTabstripPanel(
-
+      
       ##### Search Console -----------------------------------------------------------------------------------------------------------------
-
+      
       miniTabPanel(title = "Search Console",
                    miniContentPanel(
                      fluidRow(
@@ -128,11 +128,11 @@ RDMA <- function(){
       )
     )
     )
-
+  
   server <- function(input, output, session) {
-
+    
     temp_err <- reactiveValues()
-
+    
     text_page <- function(text, buffer = FALSE, button = "OK"){
       if(buffer == FALSE){
         modalDialog(
@@ -144,9 +144,9 @@ RDMA <- function(){
       } else {
         modalDialog(text, easyClose = TRUE, footer = NULL)
       }
-
+      
     }
-
+    
     scfilter.func <- function(btn.num){
       vapply(X = 1:btn.num,
              FUN = function(x){
@@ -167,7 +167,7 @@ RDMA <- function(){
              },
              FUN.VALUE = character(1))
     }
-
+    
     dtfilter.func <- function(btn.num){
       vapply(X = 11:btn.num,
              FUN = function(x){
@@ -175,13 +175,13 @@ RDMA <- function(){
                operator.chr <- input[[NS(x, "operator")]]
                expression.chr <- input[[NS(x, "expression")]]
                and_or.chr <- input[[NS(x, "and_or")]]
-
+               
                if(is.null(and_or.chr)){
                  and_or.chr <- " "
                } else {
                  if(and_or.chr == "AND"){and_or.chr <- "&"} else {and_or.chr <- "|"}
                }
-
+               
                if(operator.chr == "~~"){
                  paste0(and_or.chr, " grepl('", expression.chr, "', ", dimension.chr, ") ")
                } else if(operator.chr == "=="){
@@ -198,15 +198,15 @@ RDMA <- function(){
              },
              FUN.VALUE = character(1)) %>% paste(., collapse = " ")
     }
-
+    
     ##### Search Console TAP -------------------------------------------------------------------------------------------------------------
-
+    
     sc_data.df <- reactiveValues(sc.df = NULL,
                                  colname = NULL,
                                  Error = NULL)
     filter_btn <- reactiveValues(sc_btn = 0,
                                  dt_btn = 10)
-
+    
     # 인증
     observeEvent(input$scRefresh, {
       if(!file.exists("sc.httr-oauth")){
@@ -216,25 +216,25 @@ RDMA <- function(){
         updateActionButton(session, inputId = "scRefresh", label = "Authorization : OK")
       }
     })
-
+    
     # 인증서 제거
     observeEvent(input$scremove, {
       file.remove("sc.httr-oauth")
       updateActionButton(session, inputId = "scRefresh", label = "Authorization : NO")
     })
-
+    
     # observeEvent(input$test_button, {
     #   output$InputID_View <- renderText({
     #     c(dtfilter.func())
     #   })
     # })
-
-
-
-
-
+    
+    
+    
+    
+    
     # RDMA GSC 데이터 추출 함수
-
+    
     # SC 데이터 추출
     gsc_analytics.func <- function(siteURL, startDate, endDate, dimensions, dimensionFilterExp, rowLimit, walk_data){
       gar_auth("sc.httr-oauth")
@@ -245,13 +245,15 @@ RDMA <- function(){
                          dimensions = dimensions,
                          dimensionFilterExp = dimensionFilterExp,
                          rowLimit = rowLimit,
-                         walk_data = walk_data) %>% mutate(url = siteURL)
+                         walk_data = walk_data) %>% 
+          mutate(url = siteURL) %>% 
+          filter(!is.na(date))
       },
       error = function(e){
         print(e)
       })
     }
-
+    
     gsc_analytics_error.func <- function(siteURL, startDate, endDate, dimensions, dimensionFilterExp, rowLimit, walk_data){
       temp <- gsc_analytics.func(siteURL, startDate, endDate, dimensions, dimensionFilterExp, rowLimit, walk_data)
       if(!is.null(nrow(temp))){
@@ -269,19 +271,19 @@ RDMA <- function(){
         temp <- NULL
         return(temp)
       }
-
+      
     }
-
+    
     # 데이터 추출 시 RowLimit 측정 방법
     gsc_limit_analytics.func <- function(gsc_analytics.func, siteURL, startDate, endDate, dimensions, dimensionFilterExp, walk_data){
       row_limit.num <- 5000
-
+      
       # Google SearchConsole API에서 추출하고자 하는 데이터의 양이 얼마인지 이전에 제공해주질 않아 RowLimit을 정확히 판단 할 수 없다.
       # 해당 기간 및 설정에 따른 데이터양에 따라 RowLimit을 변경시켜주어야하는데 추출을해야 확인 가능하기 때문에 이와 같이 루프를 실행한다.
       repeat{
         temp <- gsc_analytics.func(siteURL, startDate, endDate, dimensions, dimensionFilterExp, row_limit.num, walk_data)
         cat(row_limit.num, " : ",nrow(temp), "\n")
-
+        
         # GSC 데이터 추출 시 에러발생 확인 : Error 발생 시 nrow 사용하면 Null값 출력
         if(is.null(nrow(temp))){
           # 해당 문구가 출력되면 Rowlimit이 틀리다는 것이다. 해당 문구 이외엔 Rowlimit과 관계없는 에러
@@ -315,19 +317,19 @@ RDMA <- function(){
             }
           }
         }
-
+        
       }
     }
-
+    
     # GSC 일별 추출이며 byBatch 기준으로 최대한 많은 데이터 추출
     daily_analytics.loop <- function(gsc_analytics.func, siteURL, startDate, endDate, dimensions, dimensionFilterExp, walk_data, min_row.log){
       date_range.vec <- seq(as.Date(startDate), as.Date(endDate), "days")
-
+      
       if(min_row.log){
         temp.func <- function(siteURL, daily.date, dimensions, dimensionFilterExp, rowLimit, walk_data){
           gsc_analytics_error.func(siteURL, daily.date, daily.date, dimensions, dimensionFilterExp, rowLimit, walk_data)
         }
-
+        
         lapply(X = date_range.vec,
                FUN = temp.func,
                siteURL = siteURL,
@@ -339,7 +341,7 @@ RDMA <- function(){
         temp.func <- function(gsc_analytics.func, siteURL, daily.date, dimensions, dimensionFilterExp, walk_data){
           gsc_limit_analytics.func(gsc_analytics.func, siteURL, daily.date, daily.date, dimensions, dimensionFilterExp, walk_data)
         }
-
+        
         lapply(X = date_range.vec,
                FUN = temp.func,
                gsc_analytics.func = gsc_analytics.func,
@@ -348,22 +350,22 @@ RDMA <- function(){
                dimensionFilterExp = dimensionFilterExp,
                walk_data = 'byBatch') %>% do.call(., what = rbind) %>% replace(is.na(.), 0)
       }
-
+      
     }
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
     # SC 필터 추가
     observeEvent(input$scfilteradd, {
       if(filter_btn$sc_btn < 5){
         filter_btn$sc_btn <- filter_btn$sc_btn + 1
         btn <- filter_btn$sc_btn
-
+        
         callModule(variablesServer, btn, c("country","device","page","query","searchAppearance"))
-
+        
         insertUI(
           selector = '#scfilter_place',
           where = "beforeEnd",
@@ -380,14 +382,14 @@ RDMA <- function(){
         #   output$InputID_View <- renderPrint({outs})
         # })
         # ========================================================================
-
+        
         # Dimension 수정에 따른 Operator 변경
         observeEvent(input[[NS(btn, "filterborder")]], {
           callModule(variablesServer_exp, btn, add_filter.func, input[[NS(btn, "filterborder")]])
         })
       }
     })
-
+    
     # SC 필터 제거
     observeEvent(input$scfilterdelete, {
       if(filter_btn$sc_btn > 0){
@@ -397,25 +399,25 @@ RDMA <- function(){
         filter_btn$sc_btn <- filter_btn$sc_btn - 1
       }
     })
-
+    
     # Data 필터 추가
     observeEvent(input$dtfilteradd, {
       filter_btn$dt_btn <- filter_btn$dt_btn + 1
       data_btn <- filter_btn$dt_btn
-
+      
       callModule(variablesServer, data_btn, sc_data.df$colname, FALSE)
-
+      
       insertUI(
         selector = '#datafilter_place',
         where = "beforeEnd",
         ui = variablesUI(data_btn, FALSE)
       )
-
+      
       if(data_btn >= 12){
         callModule(variablesServer_and_or, data_btn)
       }
     })
-
+    
     # Data 필터 제거
     observeEvent(input$dtfilterdelete, {
       if(filter_btn$dt_btn > 10){
@@ -425,7 +427,7 @@ RDMA <- function(){
         filter_btn$dt_btn <- filter_btn$dt_btn - 1
       }
     })
-
+    
     # 데이터 추출
     observeEvent(input$scstart, {
       element_null_ck(input$scwebsite, input$scdimension, element_name = c("Web Site URL", "Dimension"), text_page = text_page, exr = {
@@ -487,7 +489,7 @@ RDMA <- function(){
         if(!is.null(sc_data.df$Error)){output$scfail <- renderText({paste0("Fail URL \n",paste(unique(sc_data.df$Error), collapse = "\n"))})} else {output$scfail <- renderText({})}
       })
     })
-
+    
     # 데이터 전처리
     observeEvent(input$dfstart, {
       tryCatch({
@@ -496,21 +498,21 @@ RDMA <- function(){
         NULL
       })
     })
-
+    
     # 데이터 중복 처리
     observeEvent(input$dfunique, {
       groub.vec <- sc_data.df$colname[sc_data.df$colname %in% c("date", "country", "device", "page", "query", "countryName", "url")]
       sc_data.df$sc.df <- eval(parse(text = paste0("sc_data.df$sc.df %>% mutate(totalposition = clicks * impressions) %>% group_by(",
                                                    paste0(groub.vec, collapse = ", "), ") %>% summarise(clicks = sum(clicks), impressions = sum(impressions), ctr = clicks/impressions, position = sum(totalposition)/impressions)")))
     })
-
+    
     # 데이터 추출
     output$`sc_data.xlsx` <- downloadHandler(filename = function(){''},
                                              content = function(file){write.xlsx(sc_data.df$sc.df, file, row.names = FALSE)})
-
+    
   }
-
+  
   viewer <- dialogViewer("RDMA", width = 1200, height = 800)
   shiny::runGadget(ui, server, viewer = viewer)
-
+  
 }
